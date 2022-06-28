2b6c8e1f2af9605f7a7f00b21706d85b50f2978c
maisalira <mtl1@aluno.ifal.edu.br>
2022-06-28 19:17:01 +0000

    Add restrições.

--- api/SomaHandler.java
+++ api/SomaHandler.java
@@ -11,14 +11,21 @@ public class DivHandler implements HttpHandler{
 
     public void handle(HttpExchange conn) throws IOException {
        
-        String[] partes = conn.getRequestURI().getPath().split("/");
-
-        // partes { partes[0] = "", partes[1] = "soma", partes[2] = "number_1", partes[3] = "number_2"    
-        String parcela1 = partes[2];
-        String parcela2 = partes[3];
-
-        byte[] result = calculateSoma(parcela1, parcela2);
         try {
+            String[] partes = conn.getRequestURI().getPath().split("/");
+
+            if (partes.length > 4){
+                throw new IOException("Tem mais de dois parâmetros.");
+            }
+            else if (partes.length < 4){
+                throw new IOException("Tem menos de dois parâmetros.");
+            }
+            // partes { partes[0] = "", partes[1] = "soma", partes[2] = "number_1", partes[3] = "number_2"    
+            String parcela1 = partes[2];
+            String parcela2 = partes[3];
+
+            byte[] result = calculateSoma(parcela1, parcela2);
+
             conn.sendResponseHeaders(200, result.length);
 
             Headers headers = conn.getResponseHeaders();
@@ -29,7 +36,10 @@ public class DivHandler implements HttpHandler{
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
-        } catch (Exception e) {
+        } catch(NumberFormatException ex){
+            ex.printStackTrace();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         } finally {
             conn.close();
@@ -40,9 +50,6 @@ public class DivHandler implements HttpHandler{
         double dParcela1 = Double.parseDouble(parcela1);
         double dParcela2 = Double.parseDouble(parcela2);
 
-       
-
-     
         double soma = (dParcela1 / dParcela2);
 
         return Double.toString(soma).getBytes();
