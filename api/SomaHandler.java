2949a0e7526cfd6c05fa796f6bfaeb423f38f140
maisalira <mtl1@aluno.ifal.edu.br>
2022-06-28 19:20:14 +0000

    Att class name.

--- api/SomaHandler.java
+++ api/SomaHandler.java
@@ -5,7 +5,7 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class DivHandler implements HttpHandler{
+public class SomaHandler implements HttpHandler{
 
     // public static final String PATH = "/soma";
 
